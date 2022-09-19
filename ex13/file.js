22018d9627165a1f2d6db8fb9d0e77d3194b1541
Soukaina Ouchen <souchen@e2r8p1.1337.ma>
2022-09-19 11:06:49 +0100

    ex14

--- ex13/file.js
+++ ex13/file.js
@@ -14,6 +14,7 @@ Use destructuring assignment with the rest parameter to perform an effective Arr
 
 
 const source = [1,2,3,4,5,6,7,8,9,10];
+
 function removeFirstTwo(list) {
   // Only change code below this line
   const [a, b, ...arr] = list; // Change this line
